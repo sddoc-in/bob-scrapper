4bf3e34097ee12bc4332fe0422d9ae25f380db66
Akash Deep <akashdeep@Akashs-MacBook-Air.local>
2023-11-30 03:11:46 +0530

    added baseurl

--- src/constant/data.ts
+++ src/constant/data.ts
@@ -1,2 +1,3 @@
 // export const BASE_API_URL = "https://bob.sddoc.in/getdata";
-export const BASE_API_URL = "http://192.168.29.124/getdata";
+// export const BASE_API_URL = "http://192.168.29.124/getdata";
+export const BASE_API_URL = "https://effective-space-xylophone-qjr476gw6rrfx66x-5000.app.github.dev/getdata";
