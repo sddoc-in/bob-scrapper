531e729c64970da203d8bd4230f84fe64d88dd98
Deepak <60574055+deepakstark01@users.noreply.github.com>
2023-12-03 12:32:02 +0000

    latest getpartner

--- src/constant/data.ts
+++ src/constant/data.ts
@@ -1,5 +1,5 @@
-export const BASE_API_URL = "https://getapi-henna.vercel.app/getdata?";
-// export const BASE_API_URL = "https://bob.sddoc.in/getdata?";
+// export const BASE_API_URL = "https://getapi-henna.vercel.app/getdata?";
+export const BASE_API_URL = "https://bob.sddoc.in/getdata?";
 
 export const corsProxy = 'https://corsproxy.io/?';
 
--- src/excel-component/GetPartner.tsx
+++ src/excel-component/GetPartner.tsx
@@ -5,12 +5,12 @@ import { BASE_API_URL, corsProxy } from '../constant/data';
 export default async function GetNumberOfPartners(url: string) {
     try {
         // const response = await fetch(url);
-        // const response = await fetch(corsProxy + encodeURI(url));
-        const response = await fetch(`https://proxy.cors.sh/${url}`, {
-            headers: {
-              "x-cors-api-key": "temp_17866fb10689a4d54b9c6d05eb9d2a25",
-            },
-          });
+        const response = await fetch(corsProxy + encodeURI(url));
+        // const response = await fetch(`https://proxy.cors.sh/${url}`, {
+        //     headers: {
+        //       "x-cors-api-key": "temp_17866fb10689a4d54b9c6d05eb9d2a25",
+        //     },
+        //   });
         const html = await response.text();
         const $ = cheerio.load(html);
         const numOfPartnersElement = $('.buy-block__alternative-sellers-card__title:contains("partners")');
