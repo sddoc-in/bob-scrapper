2831008b1a2700e80d270b89295fd98c4cd51004
Deepak <115339084+rohitadarsh16@users.noreply.github.com>
2023-12-06 00:28:14 +0000

    done

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def index():
         <h1 class="text-3xl font-bold text-center mt-8 mb-6">Welcome Bob to the Bol.com Scraper</h1>
 
         <div class="flex justify-center">
-            <a href="/getdata?page=1&querry=charger"
+            <a href="/getdata?minpage=1&maxpage=4&querry=charger"
                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Get Data
             </a>
